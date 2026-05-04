6e395dd13f660fdf19a2767ff65258447cf66b10
Rupam Debnath <rupamdebnath627@gmail.com>
2026-05-04 12:21:24 +0530

    added index postions for both files

--- main.go
+++ main.go
@@ -1,14 +1,51 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	// Match index positions
+	const (
+		id            = 0
+		season        = 1
+		city          = 2
+		date          = 3
+		team1         = 4
+		team2         = 5
+		tossWinner    = 6
+		tossDecision  = 7
+		result        = 8
+		dlApplied     = 9
+		winner        = 10
+		winsByRun     = 11
+		winsByWicket  = 12
+		playerOfMatch = 13
+		venue         = 14
+		umpire1       = 15
+		umpire2       = 16
+		umpire3       = 17
+	)
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
-	}
+	// Delivery index positions
+	const (
+		matchID         = 0
+		inning          = 1
+		battingTeam     = 2
+		bowlingTeam     = 3
+		over            = 4
+		ball            = 5
+		batsman         = 6
+		nonStriker      = 7
+		bowler          = 8
+		isSuperOver     = 9
+		wideRuns        = 10
+		byRuns          = 11
+		legByRuns       = 12
+		noBallRuns      = 13
+		penaltyRuns     = 14
+		batsmanRuns     = 15
+		extraRuns       = 16
+		totalRuns       = 17
+		playerDismissed = 18
+		dismissalKind   = 19
+		fielder         = 20
+	)
+
 }
